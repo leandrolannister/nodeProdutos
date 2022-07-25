64617f8d840325cd12e1813ba802af609e0afcc1
hendrix <leandrohendrix@gmail.com>
2022-07-25 19:17:55 -0300

    Middleware

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ app.use(bodyparse.json()); //To api
 app.use('/', homeRoute);
 app.use('/produtos', produtosRoute);
 
+//Exemplo de middleware, quando a rota não existe
+app.use((req,res,next) => {
+    res.status(404).render('errors/404');
+});
+
+//Em caso de error essa função será executada antes da anterior
+app.use((error,req,res,next) => {
+    res.status(500).render('errors/500');
+    next();
+});
+
 app.listen(3000, () => {
     console.log('Server is running');
 }); 
